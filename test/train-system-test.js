b849a73e36f758eb7fdc8f5137ea8a6dfa74f3e8
Renan Dias <renansdiaz@gmail.com>
2015-04-20 17:41:26 -0300

    Enhanced getNextLocationForTrain method implementation

--- test/train-system-test.js
+++ test/train-system-test.js
@@ -8,16 +8,21 @@ stationNetwork.on('graphBuilt', function() {
 	console.log('Graph was BUILT!!!!');
 
 	var train = {
+		line: 'red',
 		location: {
-			latitude: -23.549373,
-			longitude: -46.623757
+			latitude: -23.525756,
+			longitude: -46.665792
 		},
 		direction: 'forward'
 	};
 
 	var ret = trainSystem.getNextLocationForTrain(train);
 
-	console.log('Returned location index: ' + ret);
+	console.log('Next Latitude: ' + ret.location.latitude);
+	console.log('Next Longitude: ' + ret.location.longitude);
+	console.log('Next Direction: ' + ret.direction);
 
-	trainSystem.getClosestTrainTo("0", "10");
+	process.exit(0);
+
+	//trainSystem.getClosestTrainTo("0", "70", function(){});
 });
