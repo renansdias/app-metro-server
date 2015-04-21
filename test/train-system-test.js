679f21b7ff4ec237a87e623524b37bbcfc4faad9
Renan Dias <renansdiaz@gmail.com>
2015-04-21 19:39:23 -0300

    Closest train to extreme stations are now being returned

--- test/train-system-test.js
+++ test/train-system-test.js
@@ -1,28 +1,33 @@
-var StationNetwork = require('../lib/station-network');
-var TrainSystem = require('../lib/train-system');
+setTimeout(function() {
+	var StationNetwork = require('../lib/station-network');
+	var TrainSystem = require('../lib/train-system');
 
-var stationNetwork = new StationNetwork();
-var trainSystem = new TrainSystem(stationNetwork);
+	var stationNetwork = new StationNetwork();
+	var trainSystem = new TrainSystem(stationNetwork);	
 
-stationNetwork.on('graphBuilt', function() {
-	console.log('Graph was BUILT!!!!');
+	stationNetwork.on('graphBuilt', function() {
+		console.log('Graph was BUILT!!!!');
 
-	var train = {
-		line: 'red',
-		location: {
-			latitude: -23.525756,
-			longitude: -46.665792
-		},
-		direction: 'forward'
-	};
+		// var train = {
+		// 	line: 'red',
+		// 	location: {
+		// 		latitude: -23.525756,
+		// 		longitude: -46.665792
+		// 	},
+		// 	direction: 'forward'
+		// };
 
-	var ret = trainSystem.getNextLocationForTrain(train);
+		// var ret = trainSystem.getNextLocationForTrain(train);
 
-	console.log('Next Latitude: ' + ret.location.latitude);
-	console.log('Next Longitude: ' + ret.location.longitude);
-	console.log('Next Direction: ' + ret.direction);
+		// console.log('Next Latitude: ' + ret.location.latitude);
+		// console.log('Next Longitude: ' + ret.location.longitude);
+		// console.log('Next Direction: ' + ret.direction);
 
-	process.exit(0);
-
-	//trainSystem.getClosestTrainTo("0", "70", function(){});
-});
+		setTimeout(function() {
+			trainSystem.getClosestTrainTo("1700", "1400", function(train) {
+				console.log(train);
+				process.exit(0);
+			});
+		}, 5000);
+	});
+}, 5000);
