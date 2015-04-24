035d88b7b94c7a424d1bd8bc291a227e33dfe6ea
Renan Dias <renansdiaz@gmail.com>
2015-04-24 19:09:20 -0300

    Server is retrieving closest train

--- test/train-system-test.js
+++ test/train-system-test.js
@@ -1,33 +1,16 @@
-setTimeout(function() {
-	var StationNetwork = require('../lib/station-network');
-	var TrainSystem = require('../lib/train-system');
+var StationNetwork = require('../lib/station-network');
+var TrainSystem = require('../lib/train-system');
 
-	var stationNetwork = new StationNetwork();
-	var trainSystem = new TrainSystem(stationNetwork);	
+var stationNetwork = new StationNetwork();
+var trainSystem = new TrainSystem(stationNetwork);	
 
-	stationNetwork.on('graphBuilt', function() {
-		console.log('Graph was BUILT!!!!');
+stationNetwork.on('graphBuilt', function() {
+	console.log('Graph was BUILT!!!!');
 
-		// var train = {
-		// 	line: 'red',
-		// 	location: {
-		// 		latitude: -23.525756,
-		// 		longitude: -46.665792
-		// 	},
-		// 	direction: 'forward'
-		// };
-
-		// var ret = trainSystem.getNextLocationForTrain(train);
-
-		// console.log('Next Latitude: ' + ret.location.latitude);
-		// console.log('Next Longitude: ' + ret.location.longitude);
-		// console.log('Next Direction: ' + ret.direction);
-
-		setTimeout(function() {
-			trainSystem.getClosestTrainTo("500", "1700", function(train) {
-				console.log(JSON.stringify(train));
-				process.exit(0);
-			});
-		}, 5000);
-	});
-}, 10000);
+	setTimeout(function() {
+		trainSystem.getClosestTrainTo("1722", "2205", function(train) {
+			console.log(JSON.stringify(train));
+			process.exit(0);
+		});
+	}, 0);
+});
