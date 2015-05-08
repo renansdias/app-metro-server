85aed752fdbd9ceb64bfc0fbeb1e5a80840657a7
Renan Dias <renansdiaz@gmail.com>
2015-05-08 18:38:33 -0300

    Fixed bug of older trainId being returned

--- servers/location-change-detector-server.js
+++ servers/location-change-detector-server.js
@@ -3,6 +3,9 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var MongoOplog = require('mongo-oplog');
+var oplog = MongoOplog('mongodb://localhost:27017/local', 'appmetro.trains').tail();
+
+global.trainId;
 
 var locationChangeDetector = function createLocationChangeDetectorServer(options) {
 	var port = options.port || 3812;
@@ -12,13 +15,16 @@ var locationChangeDetector = function createLocationChangeDetectorServer(options
 	});
 
 	io.on('connection', function(socket) {
-		var oplog = MongoOplog('mongodb://localhost:27017/local', 'appmetro.trains').tail();
+		console.log('There is a new connection!!!');
 
 		socket.on('trackTrain', function(trainId) {
-			console.log('Tracking trainId: ' + trainId);
+			global.trainId = trainId;
+			console.log('Tracking trainId: ' + global.trainId);
 			
 			oplog.on('update', function(doc) {
-				if (doc.o2._id === trainId) {
+				if (doc.o2._id === global.trainId) {
+					console.log('EMITTING EVENT TO TRAIN ID: ' + trainId);
+
 					var latitude = doc.o['$set']['location.latitude'];
 					var longitude = doc.o['$set']['location.longitude'];
 					socket.emit('trainLocationUpdate', latitude, longitude);
@@ -26,9 +32,11 @@ var locationChangeDetector = function createLocationChangeDetectorServer(options
 			});
 		});
 
-		socket.on('disconnect', function() {
-			oplog.stop();
-		});
+		// socket.on('disconnect', function() {
+		// 	// oplog.stop();
+		// 	console.log('Connection CLOSED!!!');
+		// 	socket.removeAllListeners();
+		// });
 	});
 }
 
